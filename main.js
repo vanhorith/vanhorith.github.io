c84996f7c5342c0a15938e6a9e989a7c4796811b
root <root@DESKTOP-5G5C8PO.localdomain>
2021-11-14 02:04:15 -0800

    Updates

--- main.js
+++ main.js
@@ -1,45 +1,90 @@
 // “Characteristics of pattern formation and evolution in approximations of Physarum transport networks.”
 // https://uwe-repository.worktribe.com/output/980579
 
+// Gaussian kernel for diffusion of each trail
+const gaussianFilter = [
+	1/16, 1/8, 1/16,
+	 1/8, 1/4,  1/8,
+	1/16, 1/8, 1/16,
+];
+
 let W = window.innerWidth;
 let H = window.innerHeight;
-
-// dat.GUI
-let agentsSettings = function(){
-    this.sensorOffset = 50;              // Sensor offset distance
-	this.sensorAngle = 22.5/180*Math.PI; // Sensor angle from forward position
-	this.turnSpeed = 22.5/180*Math.PI;   // A turning rate for each agent
-	this.speed = 3;                      // A speed for each agent
-	this.decayT = 0.95;                  // Trail-map chemoattractant diffusion decay factor
-	this.depositT = 5;                       // Chemoattractant deposition per step
-	this.numAgents = 5000;               // Number of agents
-
-	this.startCenter = false;            // Starts each agent in the center of the screen
-}//Agents()
-
-let settings = new agentsSettings();
 let img;
 const agents = [];
 let trail;
+let shouldRegenerate = true;
+let paused = false;
+
+
+// dat.GUI
+let agentsSettings = function(){
+	this.numAgents    = 4000;           // Number of agents
+
+    this.sensorOffset = 10;             // Sensor offset distance
+	this.sensorAngle  = 40/180*Math.PI; // Sensor angle from forward position in radians
+	
+	this.turnSpeed    = 40/180*Math.PI; // A turning rate for each agent in radians
+	this.speed        = 3;              // A speed for each agent
+	
+	this.decayT       = 0.95;           // Trail-map chemoattractant diffusion decay factor
+	this.depositT     = 1.33;           // Chemoattractant deposition per step
+
+	this.rParticle    = 255;            // Red channel (Particle)
+	this.gParticle    = 255;            // Green channel (Particle)
+	this.bParticle    = 255;            // Blue channel (Particle)
+
+	this.rBackground  = 0;              // Red channel (background/trail)
+	this.gBackground  = 0;              // Green channel (background/trail)
+	this.bBackground  = 0;              // Blue channel (background/trail)
+
+	this.startCenter  = false;          // Starts each agent in the center of the screen
+	this.pause        = false;          // Pause the main loop
+}//Agents()
+let settings = new agentsSettings();
 
 
 
 function setup(){
 	createCanvas(W, H);
+	background(0);
 	trail = new Float32Array(W * H);
 	img = createGraphics(W, H);
 	img.pixelDensity(1);
 	
 	let gui = new dat.GUI();
-	gui.add(settings, 'sensorOffset', 1, 100);
-	gui.add(settings, 'sensorAngle', 0.1, PI);
-	gui.add(settings, 'turnSpeed', 0.1, PI);
-	gui.add(settings, 'speed', 0, 15);
-	gui.add(settings, 'depositT', 0, 25);
-	gui.add(settings, 'decayT', 0, 1);
-	gui.add(settings, 'numAgents', 1, 20000);
+
+	gui.add(settings, 'numAgents'   , 1  , 20000);
+
+	var f1 = gui.addFolder('Sensor Actions');
+	f1.add(settings, 'sensorOffset', 1  , 100  );
+	f1.add(settings, 'sensorAngle' , 0.1, PI   );
+	f1.open();
+
+	var f2 = gui.addFolder('Speed Actions');
+	f2.add(settings, 'turnSpeed'   , 0.1, PI   );
+	f2.add(settings, 'speed'       , 0  , 15   );
+	f2.open();
+
+	var f3 = gui.addFolder('Chemoattractant Deposition and Decay');
+	f3.add(settings,   'depositT'    , 0  , 10   );
+	f3.add(settings,   'decayT'      , 0  , 1    );
+	f3.open();
+
+	var f4 = gui.addFolder('Particle Color');
+	f4.add(settings, 'rParticle', 0, 255);
+	f4.add(settings, 'gParticle', 0, 255);
+	f4.add(settings, 'bParticle', 0, 255);
+	f4.open();
+
+	var f5 = gui.addFolder('Background Color');
+	f5.add(settings, 'rBackground', 0, 255);
+	f5.add(settings, 'gBackground', 0, 255);
+	f5.add(settings, 'bBackground', 0, 255);
+	f5.open();
 
 	gui.add(settings, 'startCenter');
+	gui.add(settings, 'pause').onChange(function() { pause(); });
 
 	let obj = { reset: function () { reset(); } }
 	gui.add(obj, 'reset');
@@ -51,7 +96,7 @@ function index(x, y) { return x + y * width; }//index()
 
 
 
-function compute(agents, trail, W, H){
+function compute(agents, trail, width, height){
 	for(let agent of agents){
 		function sense(theta){
 			return trail[index(
@@ -60,18 +105,16 @@ function compute(agents, trail, W, H){
 			)];
 		}//sense()
 
-		const FL = sense(settings.sensorAngle);
-		const F  = sense(0);
-		const FR = sense(-settings.sensorAngle);
-
-		const randTurnSpeed = (Math.random() * 0.25 + 1) * settings.turnSpeed;
+		const F  = sense(0);                     // Forward
+		const FL = sense(settings.sensorAngle);  // Forward left
+		const FR = sense(-settings.sensorAngle); // Forward right
 
 		if (F > FL && F > FR)
 			continue;
 		else if (FL > FR)
-			agent.randHeading += randTurnSpeed;
+			agent.randHeading += settings.turnSpeed;
 		else if (FR > FL)
-			agent.randHeading -= randTurnSpeed;
+			agent.randHeading -= settings.turnSpeed;
 		else
 			agent.randHeading += Math.round(Math.random() * 2 - 1) * settings.turnSpeed;
 	}//for
@@ -86,6 +129,7 @@ function compute(agents, trail, W, H){
 	for(let agent of agents){
 		const x = Math.round(agent.x);
 		const y = Math.round(agent.y);
+
 		if (x <= 0 || y <= 0 || x >= W-1 || y >= H-1)
 			continue;
 		trail[index(x, y)] += settings.depositT;
@@ -95,17 +139,17 @@ function compute(agents, trail, W, H){
 	for(let y = 1; y < H-1; ++y){
 		for(let x = 1; x < W-1; ++x){
 			const diffused = (
-				decayingTrail[index(x-1, y-1)] * 1/16 +
-				decayingTrail[index(x  , y-1)] * 1/8  +
-				decayingTrail[index(x+1, y-1)] * 1/16 +
+				decayingTrail[index(x-1, y-1)] * gaussianFilter[0] +
+				decayingTrail[index(x  , y-1)] * gaussianFilter[1] +
+				decayingTrail[index(x+1, y-1)] * gaussianFilter[2] +
 
-				decayingTrail[index(x-1, y)] * 1/8 +
-				decayingTrail[index(x  , y)] * 1/4 +
-				decayingTrail[index(x+1, y)] * 1/8 +
+				decayingTrail[index(x-1, y)] * gaussianFilter[3] +
+				decayingTrail[index(x  , y)] * gaussianFilter[4] + 
+				decayingTrail[index(x+1, y)] * gaussianFilter[5] +
 
-				decayingTrail[index(x-1, y+1)] * 1/16 +
-				decayingTrail[index(x  , y+1)] * 1/8  +
-				decayingTrail[index(x+1, y+1)] * 1/16
+				decayingTrail[index(x-1, y+1)] * gaussianFilter[6] +
+				decayingTrail[index(x  , y+1)] * gaussianFilter[7] +
+				decayingTrail[index(x+1, y+1)] * gaussianFilter[8]
 			);
 			trail[index(x, y)] = Math.min(1.0, diffused * settings.decayT);
 		}//for
@@ -116,17 +160,16 @@ function compute(agents, trail, W, H){
 
 
 
-let shouldRegenerate = true;
 function regenerate(){
-	agents.splice(0,agents.length); // empty list
+	agents.splice(0, agents.length);
 
 	if(settings.startCenter){
 		for(let i = 0; i < settings.numAgents; ++i){
-			const vector = 2 * Math.PI*i/settings.numAgents;
+			const vectorHeading = 2 * Math.PI*i/settings.numAgents;
 			agents.push({
 				x: W/2,
 				y: H/2,
-				randHeading: vector - Math.PI / 2,
+				randHeading: vectorHeading - Math.PI / 2,
 			});
 		}//for
 	} //if
@@ -152,35 +195,69 @@ function reset(){
 
 
 
-function render(trail){
+function pause(){
+	if(settings.pause)
+		noLoop();
+	else
+		loop();	
+}//pause()
+
+
+
+function render(){
+	const width = canvas.width;
+	const height = canvas.height;
 	img.loadPixels();
 	
 	let i = 0;
-	for(let y = 0; y < W; ++y){
-		for(let x = 0; x < H; ++x){
+	for(let y = 0; y < width; ++y){
+		for(let x = 0; x < height; ++x){
 			const trailBuffer = trail[i];
-			const curbrightness = Math.floor(trailBuffer * 255);
-	
-			img.pixels[i*4+0] = curbrightness;
-			img.pixels[i*4+1] = curbrightness;
-			img.pixels[i*4+2] = curbrightness;
-			img.pixels[i*4+3] = 255;
+			const curbrightness = Math.floor(trailBuffer * 200);
+			img.pixels[i*4+0] = curbrightness + settings.rBackground;
+			img.pixels[i*4+1] = curbrightness + settings.gBackground;
+			img.pixels[i*4+2] = curbrightness + settings.bBackground;
+			img.pixels[i*4+3] = 100;
 	
 			i++;
 		}//for
 	}//for
 
   img.updatePixels();
+  tint(settings.rParticle, settings.gParticle, settings.bParticle);
+  img.updatePixels();
 }//render()
 
 
 
-function draw(){  
-  if(shouldRegenerate)
-    regenerate();
+function draw(){
+	// background(0);
+	imageMode(CENTER);
 
-  trail = compute(agents, trail, W, H);
-  
-  render(trail);
-  image(img, 0, 0, W, H);
+	if(shouldRegenerate)
+		regenerate();
+
+	trail = compute(agents, trail, W, H);
+
+	render();
+	// img.fill(0, 255, 255);
+	image(img, W/2, H/2, W, H);
 }//draw()
+
+
+
+(function () {
+	let SSWZ = function () {
+		this.keyScrollHandler = function (e) {
+			if (e.ctrlKey) {
+				e.preventDefault();
+				return false;
+			}
+		}
+	};
+
+	if (window === top) {
+		let sswz = new SSWZ();
+		window.addEventListener('wheel', sswz.keyScrollHandler, { passive: false });
+	}
+})();
